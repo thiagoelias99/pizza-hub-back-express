7eece3eb6dba66b4ca87eb760c1e62fcd0c54aa2
Thiago Elias <thiagoelias99@gmail.com>
2023-12-14 13:34:19 -0300

    feat: added vite port in cors

--- src/server/express.ts
+++ src/server/express.ts
@@ -11,7 +11,7 @@ server.use(express.urlencoded({ extended: false }))
 
 //Cors configuration
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','http://localhost:5173'],
 }
 server.use(cors(corsOptions))
 
